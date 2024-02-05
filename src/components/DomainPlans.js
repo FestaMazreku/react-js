d5207e50a6452591ac5a0ae508e237e9452cf743
Festa <festamazreku@gmail.com>
2024-02-05 13:55:11 +0100

    Fixed the tasks

--- src/components/DomainPlans.js
+++ src/components/DomainPlans.js
@@ -21,7 +21,7 @@ const TabsComponent = ({ tabData }) => {
     return arr
       ? Array.from({ length: Math.ceil(arr.length / size) }, (v, i) =>
           arr.slice(i * size, i * size + size)
-        )
+        ) 
       : [];
   };
 
